d24b7f72031d8afa40f8a80b06e5c5e33cc0409a
dnilesh05 <niludhaneshwar@gmail.com>
2018-07-13 11:14:24 +0530

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+  import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TeamComponent } from './team/team.component';
+import {RouterModule ,Routes }from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AboutUsComponent,
+    TeamComponent,
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],    
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'', component:HomeComponent},
+      {path:'team', component:TeamComponent},
+      {path:'about',component:AboutUsComponent},
+      {path:'product',component:ProductComponent}
+    ])
   ],
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
